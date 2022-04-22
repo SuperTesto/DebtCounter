1f56536ce39584e8e15a5d7b738158755d4122db
CLleese <simeon.kozhin@gmail.com>
2022-04-22 21:19:28 +0300

    Реализована функция добавления человека

---
+++ app/src/main/java/com/example/debtcounter/Human.kt
@@ -0,0 +1,36 @@
+package com.example.debtcounter
+
+class Human {
+  private var name: String = ""
+  private var total: Double = 0.0
+  private var amount: Double = 0.0
+
+  fun setName(_name: String){
+    name = _name
+  }
+  fun getName():String{
+    return name
+  }
+
+  fun setAmount(_amount: Double){
+    amount = _amount
+  }
+  fun getAmount():Double{
+    return amount
+  }
+
+  fun setTotal(_total: Double){
+    total = _total
+  }
+  fun getTotal():Double{
+    return total;
+  }
+
+
+  fun amountInc():Double{
+    return amount.plus(0.5)
+  }
+  fun amountDec():Double{
+    return amount.minus(0.5)
+  }
+}
--- app/src/main/java/com/example/debtcounter/MainActivity.kt
+++ app/src/main/java/com/example/debtcounter/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.debtcounter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
+
+
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var buttonPlus: View
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    buttonPlus = findViewById(R.id.buttonPlus)
+
+    buttonPlus.setOnClickListener() {
+      addRow()
+    }
   }
+
+  @SuppressLint("InflateParams")
+  private fun addRow() {
+    val tl: TableLayout = findViewById(R.id.tableLayout)
+    val inflater: LayoutInflater = LayoutInflater.from(this)
+    val tr: TableRow = inflater.inflate(R.layout.table_row, null) as TableRow
+
+    tl.addView(tr)
+  }
+
+
 }
